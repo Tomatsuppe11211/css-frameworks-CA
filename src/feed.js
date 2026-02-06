f51254d6ab52ea08a4282c10069a0586774fa405
Tomatsuppe11211 <marcos.lindboe47@gmail.com>
2026-02-06 15:53:19 +0100

    Adding more content on feed page
    - Added a sidebar for desktop version and a topbar for mobile version
    
    - added functions to buttons
    
    - added a hardcoded post on the feed page
    
    - Added a button for creating a post (non-functional yet)

--- src/feed.js
+++ src/feed.js
@@ -13,3 +13,45 @@ menubarIcon.addEventListener('click', function(){
 closeMobileNavButton.addEventListener('click', function(){
     mobileNav.style.display = 'none';
 })
+
+
+
+
+//For the sidebar/topbar
+const filterButton = document.getElementById('filterButton');
+const searchButton = document.getElementById('searchButton');
+const mobileFilter = document.getElementById('mobileFilter');
+const mobileSearch = document.getElementById('mobileSearch');
+const mobileSearchButton = document.getElementById('mobileSearchButton');
+
+
+
+//Adding show/hide filter and search functions
+function showFilter(){
+    mobileFilter.style.display = 'flex';
+    filterButton.removeEventListener('click', showFilter);
+    filterButton.addEventListener('click', hideFilter);
+};
+
+function hideFilter(){
+    mobileFilter.style.display = 'none';
+    filterButton.removeEventListener('click', hideFilter);
+    filterButton.addEventListener('click', showFilter); 
+}
+
+function showSearch(){
+    mobileSearch.style.display = 'flex';
+    searchButton.removeEventListener('click', showSearch);
+    searchButton.addEventListener('click', hideSearch);
+}
+
+function hideSearch(){
+    mobileSearch.style.display = 'none'
+    searchButton.removeEventListener('click', hideSearch);
+    searchButton.addEventListener('click', showSearch);
+}
+
+
+//connecting functions to the buttons
+filterButton.addEventListener('click', showFilter);
+searchButton.addEventListener('click', showSearch);
