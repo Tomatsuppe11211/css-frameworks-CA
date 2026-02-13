b35affd614d9b8555d03d262466630ffb4413c3e
Tomatsuppe11211 <marcos.lindboe47@gmail.com>
2026-02-13 16:00:44 +0100

    Editing "active page" indicators
    Giving the profile logo the brand color on profile page so the user know they are on the profile page
    
    When watching other prolfiles, none have the brand color. indicating the user is not on one of the main pages.
    
    - Making mobile menu work on /profiles/seeProfile.html

--- src/seeProfile.js
+++ src/seeProfile.js
@@ -1,3 +1,16 @@
+//Getting clickable buttons
+const menubarIcon = document.getElementById('menubarIcon');
+const mobileNav = document.getElementById('dropdownNavbar'); 
+const closeMobileNavButton = document.getElementById('closeNavbar');
+
+menubarIcon.addEventListener('click', function(){
+    mobileNav.style.display = 'flex';
+});
+
+closeMobileNavButton.addEventListener('click', function(){
+    mobileNav.style.display = 'none';
+});
+
 const mobileProfileButton = document.getElementById('mobileProfileLogo');
 const desktopProfileButton = document.getElementById('desktopProfileLogo');
 
